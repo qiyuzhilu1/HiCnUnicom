5d0b31e4e9e95f397ec09b794e29158033fcb946
qiyuzhilu1 <49613007+qiyuzhilu1@users.noreply.github.com>
2020-03-25 21:43:44 +0800

    Update CnUnicom.sh

--- CnUnicom.sh
+++ CnUnicom.sh
@@ -9,7 +9,7 @@ export PATH
 alias curl='curl -m 10'
 
 # user info: change them to yours or use parameters instead.
-username="$1"
+username="18575595974"
 password="$2"
 
 # UA and deviceId: if you failed to login , maybe you need to change it to your IMEI.
